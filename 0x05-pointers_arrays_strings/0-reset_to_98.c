dcb11d6a9ef96f2518c9b981363e6fdfd242ea64
EZE ELISHA UCHECHUKWU <kellard4u@yahoo.com>
2023-04-04 15:14:37 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,34 +1,9 @@
-#include <stdio.h>
-#include "main.h"
 /**
  * reset_to_98 - Reset a given integer variable to the value 98.
  *
  * @n: Pointer to an integer variable to reset.
  *
- * Return: None.
- */
-void reset_to_98(int *n);
-/**
- * main - Entry point
- *
- * Description: Demonstrates the use of the reset_to_98 function.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
-/**
- * reset_to_98 - Reset a given integer variable to the value 98.
- *
- * @n: Pointer to an integer variable to reset.
+ * Return: nothing
  *
  * Description: Implement the function
  */
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void reset_to_98(int *n);
-
-#endif /* MAIN_H */
+void swap_int(int *a,int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *dest, char *src);
+int _atoi(char *s);
 
